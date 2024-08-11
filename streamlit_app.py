9f5f96fc442c76bd29c5ac9baed3b7c00ddaddea
ravikumarp1 <ravikumarp1@hexaware.com>
2024-08-11 23:41:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+# from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col,when_matched
 
 
@@ -24,8 +24,9 @@ st.write(
 # )
 
 # st.write("Your Faviorate food is:", option)
-
-session = get_active_session()
+cnx=st.connection('snowflake')
+session = cnx.session()
+# session = get_active_session()
 my_dataframe = session.table("smoothies.public.orders").filter(col("ORDER_FILLED")==0).collect()
 
 editable_df = st.data_editor(my_dataframe)
