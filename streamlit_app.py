f02f6644e19f639c5f1bfcee20105163eecbd652
ravikumarp1 <ravikumarp1@hexaware.com>
2024-08-14 12:57:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ from snowflake.snowpark.functions import col
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 # st.text(fruityvice_response)
-fv_df = st.dataframe=(data=fruityvice_response.json() , use_contanier_width=True)
+fv_df = st.dataframe=(data=fruityvice_response.json(), use_contanier_width=True)
 
 
 Name_of_order=st.text_input('Name On the Smoothie ')
